a099ee912addd0ab6ed278e7d6635164da491922
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2021-08-08 16:41:47 +0200

    Update index.test.ts

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,18 +1,68 @@
-jest.useFakeTimers()
-
 import dodb from '../index'
 
-describe("DODB", () => {
+test.skip("Module dodb Object", async () => {
 
-    it("syncDatabase", async () => {
+    let data = await dodb()
 
-        let data = await dodb()
+    expect(typeof data).toBe('object')
+});
 
-        console.log(data)
-        //expect(user.getName()).toBe('user')
+test.skip("Get user by id", async () => {
 
-        expect(200).toBe(200)
+    let { user } = await dodb()
+    let data = await user.getById(1)
 
+    expect(typeof data).toBe('object')
+});
 
-    });
+test.skip("Get user by other params", async () => {
+
+    let { user } = await dodb()
+
+    let where = {
+        name: 'Sergio'
+    }
+
+    let data = await user.get({ where })
+
+    expect(typeof data).toBe('object')
+});
+
+test.skip("Inserta datos", async () => {
+
+    let { user } = await dodb()
+
+    let params = {
+        name: 'Jessica',
+        surname: 'Amer',
+        alias: 'jamer',
+        email: 'jamer@jscode.es',
+        pass: '*****'
+    }
+
+    let data = await user.add(params)
+
+    expect(typeof data).toBe('object')
+});
+
+test.skip("Update datos by params", async () => {
+
+    let { user } = await dodb()
+
+    let find =
+    {
+        name: 'Maria'
+    }
+
+    let params = {
+        name: 'Jessica',
+        surname: 'Amer',
+        alias: 'jamer',
+        email: 'jamer@jscode.es',
+        pass: '************'
+    }
+
+    let data = await user.update(find, params)
+
+    expect(typeof data).toBe('object')
 });
