8a36136a327c312e8357d8a001ef5715bfd5a8f8
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2021-08-08 15:14:48 +0200

    Params get

--- src/core/model.ts
+++ src/core/model.ts
@@ -6,6 +6,7 @@ export default class Model {
     private name: string
     private schema: string
     private attrs: any
+    private keys: Array<String> = []
 
     constructor(schema: string, name: string) {
         this.name = name
@@ -30,6 +31,15 @@ export default class Model {
 
         let data = await db.query(sql)
 
+        let keys: Array<String> = []
+
+        for (const item of data) {
+
+            keys.push(item.COLUMN_NAME)
+        }
+
+        this.keys = keys
+
         return data
     }
 
@@ -50,7 +60,7 @@ export default class Model {
         let data = await db.query(sql)
 
         for (const item of data) {
-            
+
             primayKey.push(item.COLUMN_NAME)
         }
 
@@ -58,54 +68,102 @@ export default class Model {
     }
 
     // Data, pude un objecto o un array de objectos
-    async add(data: any) { 
+    async add(data: any) {
 
         if (typeof data !== 'object') return false
 
         let { attrs } = this
 
-        let result:any = []
-        let errors :any= []
+        let result: any = []
+        let errors: any = []
 
-        if(!Array.isArray(data))
-        {
+        if (!Array.isArray(data)) {
             data = [data]
         }
 
         for (const item of data) {
-            
+
             let { error, value } = Validate.check(attrs, item)
 
             if (!error) {
-    
+
                 let db = new Database()
-    
+
                 let keys = Object.keys(value)
 
                 let sql = `INSERT INTO \`${this.getName()}\` ( \`${keys.join("`,`")}\` ) VALUES ( :${keys.join(",:")} )`
-    
+
                 let success = await db.query(sql, value)
-    
+
                 result.push(success)
-            } else 
+            } else
                 errors.push(error)
 
         }
 
-        if(errors.length===0) errors = false
+        if (errors.length === 0) errors = false
 
-        return {result, errors}
+        return { result, errors }
     }
 
-    async get(params:any) {
+    /*
+    
+    where : {key=value}
+    
+    */
+
+    async get(params: any) {
 
         let db = new Database()
 
         let sql = `SELECT * FROM \`${this.getName()}\``
 
-        if('where' in params)
-        {
-            //TODO: continuar
+        if (typeof params === 'object') {
+
+            if ('values' in params) {
+
+                let replace: any = this.formatValue(params.values)
+
+                if (replace.length === 0) {
+                    replace = '*'
+                }
+
+                sql = sql.replace('*', replace)
+
+            }
+
+            if ('where' in params) {
+                sql += this.formatWhere(params.where, '=')
+            }
+
+            if ('in' in params) {
+
+                if ('where' in params) {
+                    sql += ` AND ${this.formatIn(params.in, true)}`
+                } else {
+                    sql += this.formatIn(params.in)
+                }
+
+            }
+
+            if ('like' in params) {
+
+                if ('where' in params) {
+                    sql += ` AND ${this.formatWhere(params.like, 'LIKE', true)}`
+                } else {
+                    sql += this.formatWhere(params.like, 'LIKE')
+                }
+            }
+
+            if ('order' in params) {
+
+                sql += this.formatOrder(params.order)
+
+            }
+
+            if ('limit' in params) {
+                sql += ` LIMIT ${params.limit}`
+            }
         }
 
         let result = await db.query(sql)
@@ -113,17 +171,16 @@ export default class Model {
         return result
     }
 
-    async getById(id:number|string, pk='id')
-    {
+    async getById(id: number | string, pk = 'id') {
+
         let primaryKey = await this.getPrimaryKey()
 
-        let result:any = []
+        let result: any = []
 
-        let where :any= {}
+        let where: any = {}
 
-        if(!primaryKey.includes(pk))
-        {
-            if(primaryKey.length!=0)
+        if (!primaryKey.includes(pk)) {
+            if (primaryKey.length != 0)
                 pk = primaryKey[0]
             else
                 return false
@@ -131,7 +188,7 @@ export default class Model {
 
         where[pk] = id
 
-        result = await this.get({where})
+        result = await this.get({ where })
 
         return result
     }
@@ -146,18 +203,17 @@ export default class Model {
 
         let where = ''
 
-        primaryKey.forEach(function(key) { 
+        primaryKey.forEach(function (key) {
 
             where += 'WHERE '
 
-            if(key in data)
-            {
+            if (key in data) {
                 where += `\`${key}\` = '${data[key]}'`
                 delete data[key]
             }
-        });
+        })
 
-        if(!isPrimaryExist) return false
+        if (!isPrimaryExist) return false
 
         let { attrs } = this
 
@@ -167,16 +223,13 @@ export default class Model {
 
             let db = new Database()
 
-            let keys = Object.keys(value)
-
             let set = ''
 
-            for (const key in value) 
-            {
-                set+=`\`${key}\` = '${value[key]}',`
+            for (const key in value) {
+                set += `\`${key}\` = '${value[key]}',`
             }
 
-            set = set.slice(0, -1) 
+            set = set.slice(0, -1)
 
             let sql = `UPDATE \`${this.getName()}\` SET ${set} ${where}`
 
@@ -190,19 +243,19 @@ export default class Model {
 
     async remove(id: any) {
 
-        
+
         if (id && typeof id !== 'object') {
 
             let db = new Database()
-    
+
             let primaryKey = await this.getPrimaryKey()
-    
+
             let sql = `
             DELETE FROM \`${this.getName()}\` 
             WHERE ${primaryKey} = :id`
 
             let result = await db.query(sql, { id })
-    
+
             return result
         }
 
@@ -218,6 +271,187 @@ export default class Model {
 
         model.setAttrs(attrs)
 
-        return model;
+        return model
+    }
+
+
+    private formatIn(_in: any, delWhere: boolean = false) {
+
+        let { keys } = this
+
+        let sql = 'WHERE'
+
+        if (delWhere) sql = ''
+
+        let existKey = false
+
+        for (const key in _in) {
+
+            if (keys.includes(key)) {
+
+                existKey = true
+
+                if (Array.isArray(_in[key])) {
+
+                    sql += ` \`${key}\` IN ( '${_in[key].join('\',\'')}' ) AND`
+
+                }
+
+            } else {
+                console.warn(`[ WARN ] This key '${key}', no exist`)
+            }
+        }
+
+        let regex = /AND$/gm;
+
+        if (regex.test(sql)) {
+            sql = sql.slice(0, -4)
+        }
+
+        if (!existKey) sql = ''
+
+        return sql
+    }
+
+    private formatOrder(order: any) {
+        let { keys } = this
+        let sql = ' ORDER BY '
+        let existKey = false
+
+        for (const key in order) {
+            if (keys.includes(key)) {
+                existKey = true
+                sql += `\`${key}\` ${order[key]},`
+            } else {
+                console.warn(`[ WARN ] This key '${key}', not exist`)
+            }
+        }
+
+        let regex = /\,$/gm;
+
+        if (regex.test(sql)) {
+            sql = sql.slice(0, -1)
+        }
+
+        if (!existKey) sql = ''
+
+        return sql
+    }
+
+    private formatValue(value: any): String {
+
+        let { keys } = this
+
+        let existKey = false
+        let sql = ''
+
+        if (Array.isArray(value)) {
+
+            for (const key of value) {
+
+                if (keys.includes(key)) {
+
+                    existKey = true
+
+                    sql += `\`${key}\`, `
+
+                } else {
+
+                    const regex = /^([a-zA-Z]+)+\(+[a-zA-Z\*]+\)/gm;
+
+                    if (regex.test(key)) {
+
+                        let [fun, alias] = key.split(':')
+
+                        alias = (alias) ? ` AS \`${alias}\`` : ''
+
+                        sql += `${fun}${alias}, `
+
+                    } else {
+
+                        let [attr, alias] = key.split(':')
+
+                        if (keys.includes(attr)) {
+
+                            existKey = true
+
+                            alias = (alias) ? ` AS \`${alias}\`` : ''
+
+                            sql += `\`${attr}\`${alias}, `
+
+                        } else {
+
+                            console.warn(`[ WARN ] This key '${key}', not exist`)
+                        }
+
+                    }
+                }
+
+            }
+        }
+
+        let regex = /\,\s$/gm;
+
+        if (regex.test(sql)) {
+            sql = sql.slice(0, -2)
+        }
+
+        if (!existKey) sql = ''
+
+        return sql
+    }
+
+    private formatWhere(where: any, type: string = '=', delWhere: boolean = false) {
+
+        let { keys } = this
+
+        let sql = 'WHERE'
+
+        if (delWhere) sql = ''
+
+        let existKey = false
+
+        for (const key in where) {
+
+            if (keys.includes(key)) {
+
+                existKey = true
+
+                if (Array.isArray(where[key])) {
+
+                    let or = ''
+
+                    for (const item of where[key]) {
+
+                        or += `\`${key}\` ${type} '${item}' OR `
+                    }
+
+                    sql += ` ( ${or.slice(0, -3)} )`
+
+                } else {
+
+                    let regex = /\)$/gm;
+
+                    if (regex.test(sql)) {
+                        sql += ' AND'
+                    }
+
+                    sql += ` \`${key}\` ${type} '${where[key]}' AND`
+                }
+
+            } else {
+                console.warn(`[ WARN ] This key '${key}', not exist`)
+            }
+        }
+
+        let regex = /AND$/gm;
+
+        if (regex.test(sql)) {
+            sql = sql.slice(0, -4)
+        }
+
+        if (!existKey) sql = ''
+
+        return sql
     }
 }
