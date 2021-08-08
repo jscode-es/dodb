8220edd5b31a1f67bcae8b540faecc5e8b47422f
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2021-08-08 15:14:29 +0200

    Remove integrate test

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
 import Build from './core/build'
 
-(async () => {
-
-    let { category } = await Build.syncDatabase()
-
-    let insert =
-    {
-        //id:1,
-        name: 'java',
-    }
- 
-    let data = await category.add(insert)
-    //let data = await category.get(2)
-    //let data = await category.update(insert)
-    //let data = await category.remove(1) 
-
-    console.log(data)
-
-})()
-
 export default async () => await Build.syncDatabase()
